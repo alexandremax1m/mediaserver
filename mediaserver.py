9fb4d211e2cc46bf0976a2d69851edc033811296
alexandremax1m <119704433+alexandremax1m@users.noreply.github.com>
2023-01-03 14:24:00 +0100

    Update mediaserver.py

--- mediaserver.py
+++ mediaserver.py
@@ -5,7 +5,6 @@ urls, plotdata = [],[]
 
 while int(input('1 To Add URL, -1 To Cancel : ')) != int(-1):
     urls.append(str(input('Your URL Sir : ')))
-    #plotdata.append([URL(x).ipinfo()['loc'].split(',') for x in urls])
 
 for n in range(len(urls)):
     plotdata.append([])
@@ -14,11 +13,3 @@ for n in range(len(urls)):
     map = folium.Map(location=plotdata[n])
     map.add_child(folium.Marker(location=plotdata[n]))
     map.save(f'{URL(urls[n]).url[4:-4]}.html')
-
-    
-    
-    #plotdata[n][0] = [float(x) for x in plotdata[n][0]]
-    #foliummap = folium.Map(location=plotdata[n][0])
-    #foliummap.add_child(folium.Marker(location=plotdata[n][0]))
-    #foliummap.save(f'{URL(urls[n]).url[4:-4]}.html')
-    #print(plotdata[n][0])
